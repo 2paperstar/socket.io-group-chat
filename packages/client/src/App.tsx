2fb095d89637179bbcc383564eb19ffa8189d223
2paperstar <lee@paperst.ar>
2024-11-05 20:16:30 +0900

    chore: cleanup assets, use tailwind with pretendard

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,8 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { io, Socket } from 'socket.io-client';
+import { useEffect } from 'react';
 import { ClientToServerEvents, ServerToClientEvents } from 'shared';
+import { io, Socket } from 'socket.io-client';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     const sio: Socket<ServerToClientEvents, ClientToServerEvents> = io(
       'http://localhost:3000',
@@ -22,19 +17,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
