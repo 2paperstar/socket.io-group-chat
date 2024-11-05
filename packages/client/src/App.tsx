c5690faca434eea57a80a20a6e488ce3f9cd842a
2paperstar <lee@paperst.ar>
2024-11-06 08:04:18 +0900

    feat: apply react router

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,36 +1,17 @@
-import { CreateRoomButton } from './components/CreateRoomButton';
-import { useSocket } from './hooks/useSocket';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 function App() {
-  const { rooms } = useSocket();
-
-  return (
-    <main className="flex min-h-screen flex-col p-2 gap-2">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Group Chat</h1>
-        <CreateRoomButton />
-      </div>
-      <div className="border border-black flex-1">
-        {rooms.map((room) => (
-          <div
-            key={room.id}
-            className="p-2 border-b border-gray-300 flex justify-between items-center"
-          >
-            <div className="flex flex-col">
-              <div className="text-lg">{room.name}</div>
-              <div className="text-slate-600">{room.description}</div>
-            </div>
-            <button
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-              onClick={() => {}}
-            >
-              Join
-            </button>
-          </div>
-        ))}
-      </div>
-    </main>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ packages/client/src/routes/index.tsx
@@ -0,0 +1,39 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { useSocket } from '../hooks/useSocket';
+import { CreateRoomButton } from '../components/CreateRoomButton';
+
+const ListPage = () => {
+  const { rooms } = useSocket();
+
+  return (
+    <main className="flex min-h-screen flex-col p-2 gap-2">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Group Chat</h1>
+        <CreateRoomButton />
+      </div>
+      <div className="border border-black flex-1">
+        {rooms.map((room) => (
+          <div
+            key={room.id}
+            className="p-2 border-b border-gray-300 flex justify-between items-center"
+          >
+            <div className="flex flex-col">
+              <div className="text-lg">{room.name}</div>
+              <div className="text-slate-600">{room.description}</div>
+            </div>
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              onClick={() => {}}
+            >
+              Join
+            </button>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export const Route = createFileRoute('/')({
+  component: ListPage,
+});
