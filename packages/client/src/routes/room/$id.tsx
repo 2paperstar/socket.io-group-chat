3b25bcb0c4ddc583fd1f10e01e23cf31b6adfeb2
2paperstar <lee@paperst.ar>
2024-11-09 23:17:36 +0900

    feat: show room information

--- packages/client/src/routes/room/$id.tsx
+++ packages/client/src/routes/room/$id.tsx
@@ -8,7 +8,7 @@ import { useSocket } from '../../hooks/useSocket';
 import { MessageOnServer } from 'shared';
 
 const useRoom = (id: string) => {
-  const { joinRoom, leaveRoom, messages, sendMessage } = useSocket();
+  const { joinRoom, leaveRoom, messages, sendMessage, rooms } = useSocket();
 
   useEffect(() => {
     joinRoom(id);
@@ -21,6 +21,7 @@ const useRoom = (id: string) => {
     id,
     messages,
     sendMessage,
+    room: rooms.find((room) => room.id === id),
   };
 };
 
@@ -42,7 +43,7 @@ const scheme = z.object({
 
 const ChatRoomPage = () => {
   const { id } = Route.useParams();
-  const { messages, sendMessage } = useRoom(id);
+  const { messages, sendMessage, room } = useRoom(id);
   const { register, handleSubmit, setValue } = useForm<z.infer<typeof scheme>>({
     resolver: zodResolver(scheme),
   });
@@ -55,6 +56,17 @@ const ChatRoomPage = () => {
   return (
     <main className="flex flex-col p-2 gap-2 flex-1 h-0">
       <div className="border border-black flex-1 p-2 flex flex-col gap-2 h-0">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl">Room: {room?.name}</h1>
+          <button
+            className="flex gap-2 items-center px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+            onClick={() => {
+              window.history.back();
+            }}
+          >
+            Leave
+          </button>
+        </div>
         <div className="flex flex-1 flex-col-reverse border border-black p-2 gap-2 overflow-y-scroll">
           {messages
             .toReversed()
